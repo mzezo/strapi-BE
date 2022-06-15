5a29a5c843f909e2ebf41a4fe65eda303082463d
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-06-15 22:44:00 +0200

    set rejectUnauthorized to false

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ module.exports = ({ env }) => {
           user: config.user,
           password: config.password,
           ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            rejectUnauthorized: false,
           },
         },
         debug: false,
